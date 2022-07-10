3997b6bbc4e8186570f318e02b6f755b9a0410a0
priyanshu87694 <priyanshu87694@gmail.com>
2022-07-10 19:44:27 +0530

    requestRandomWinner

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,15 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
     uint256 private immutable i_entranceFee;
     address payable[] public s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
-    constructor (uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter (address indexed player);
+    event RequestedRaffleWinner (uint256 indexed requestId);
+        
+    constructor (
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+        ) VRFConsumerBaseV2 (vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle () public payable {
@@ -17,9 +40,23 @@ contract Raffle {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner () {}
+    function requestRandomWinner () external  {
+        uint256 requestId  = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords (uint256 requestId, uint256[] memory randomWords) internal override {}
+    
+    /* View Function */
 
     function getEntranceFee () public view returns (uint256) {
         return i_entranceFee;
