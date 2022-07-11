07b75ee3738185488d77bea7a99f4af5204ecca1
priyanshu87694 <priyanshu87694@gmail.com>
2022-07-11 14:00:03 +0530

    fulfillRandomWords

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -5,6 +5,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
@@ -17,9 +18,13 @@ contract Raffle is VRFConsumerBaseV2 {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    /* Lottery Variables */
+    address private s_recentWinner;
+
     /* Events */
     event RaffleEnter (address indexed player);
     event RequestedRaffleWinner (uint256 indexed requestId);
+    event WinnerPicked (address indexed winner);
         
     constructor (
         address vrfCoordinatorV2,
@@ -54,7 +59,16 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords (uint256 requestId, uint256[] memory randomWords) internal override {}
+    function fulfillRandomWords (uint256 /*requestId*/, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked (recentWinner);
+    }
     
     /* View Function */
 
@@ -64,6 +78,10 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer (uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner () public view returns (address) {
+        return s_recentWinner;
     }
 
 }
